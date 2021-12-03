4816dcd533f9b8debeef13156eabead6fde1d253
Skaran1 <44039521+Skaran1@users.noreply.github.com>
2021-12-03 23:12:06 +0300

    Add files via upload

---
+++ problems/02/d_flip_flop.v
@@ -0,0 +1,11 @@
+module d_flip_flop
+(
+    input wire d,
+    input wire clk,
+    output wire q,
+    output wire nq
+);
+wire s0;
+d_latch my_sr_l(.d(d), .e(~e), .q(s0), .nq());
+d_latch my_sr_l(.d(s0), .e(e), .q(q), .nq(nq));
+endmodule
